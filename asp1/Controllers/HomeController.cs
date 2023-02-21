f0c7afcf4cb38bdf1dd4f87a9dc83f3bec2fffc5
Farid0558475484 <faridmh@code.edu.az>
2023-02-21 17:03:37 +0400

    feat:add Model:Students,Group and Add bootstrap link

--- asp1/Controllers/HomeController.cs
+++ asp1/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using asp1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace asp1.Controllers
@@ -17,14 +18,23 @@ namespace asp1.Controllers
             //});
             //return File("~/img/lorem.jpeg", "image/jpeg");
 
-
-            return View();
+            var students = GetStudents();
+            return View(students);
         }
 
 
         public IActionResult About()
         {
             return View();
+        }
+
+        private List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Id = 1,Name="Farid",Surname="Hacizade"});
+            students.Add(new Student() { Id = 2, Name = "Elnar", Surname = "Memmedov" });
+            students.Add(new Student() { Id = 2, Name = "Togrul", Surname = "Gurbanli" });
+            return students;
         }
     }
 }
---
+++ asp1/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace asp1.Models
+{
+	public class Group
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
